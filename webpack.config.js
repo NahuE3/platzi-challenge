038fb0bec2b92bbfc17f5dc9e93d8dd039abbdc1
NahuE3 <nahueldani@gmail.com>
2021-03-05 09:28:44 -0300

    Configuracion PWA

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const WebpackPwaManifestPlugin = require('webpack-pwa-manifest');
+const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -53,6 +55,46 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: 'assets/[name].css',
     }),
+    new WebpackPwaManifestPlugin({
+      name: 'Foody+ | Las mejores y mas deliciosas recetas',
+      short_name: 'Foody+',
+      description: 'Foody+ te ayuda a elegir y preparar tus mejores comidas.',
+      background_color: '#ffffff',
+      theme_color: '#2196f3',
+      crossorigin: 'use-credentials', 
+      icons: [
+        {
+          src: path.resolve('src/assets/icons/icon.png'),
+          sizes: [96, 128, 192, 256, 384, 512] 
+        }      
+      ]
+    }),
+    new WorkboxWebpackPlugin.GenerateSW({
+      swDest: 'service-worker.js',
+      clientsClaim: true,
+      skipWaiting: true,
+      maximumFileSizeToCacheInBytes: 5000000,
+      runtimeCaching: [
+        {
+          urlPattern: new RegExp(
+            'https://(res.cloudinary.com|images.unsplash.com)'
+          ),
+          handler: 'CacheFirst',
+          options: {
+            cacheName: 'images'
+          }
+        },
+        {
+          urlPattern: new RegExp(
+            'https://foodyapi.xyz'
+          ),
+          handler: 'NetworkFirst',
+          options: {
+            cacheName: 'api'
+          }
+        }
+      ]
+    })
   ],
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
