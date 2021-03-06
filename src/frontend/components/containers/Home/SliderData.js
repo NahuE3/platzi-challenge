7fe64e4c2d43504ecf99e825dc24a7a444369419
Nahuel <37487823+NahuE3@users.noreply.github.com>
2021-03-06 02:24:40 -0300

    Update SliderData.js

--- src/frontend/components/containers/Home/SliderData.js
+++ src/frontend/components/containers/Home/SliderData.js
@@ -1,22 +1,30 @@
-export const SliderData = [
-    {
-      image:
-        'https://images.unsplash.com/photo-1513104890138-7c749659a591?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8cGl6emF8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
-    },
-    {
-      image:
-        'https://images.unsplash.com/photo-1498837167922-ddd27525d352?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8Zm9vZHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
-    },
-    {
-      image:
-        'https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixid=MXwxMjA3fDB8MHxzZWFyY2h8M3x8Zm9vZHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
-    },
-    {
-      image:
-        'https://images.unsplash.com/photo-1585238342024-78d387f4a707?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTN8fHBpenphfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
-    },
-    {
-      image:
-        'https://images.unsplash.com/photo-1590947132387-155cc02f3212?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTJ8fHBpenphfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
-    }
-  ];
+export const SlidesMobile = [
+      {
+        image:
+          'https://dl.dropboxusercontent.com/s/kir8ssl4ylsgpep/Banner_smartphone_1.png?dl=0',
+      },
+      {
+        image:
+          'https://dl.dropboxusercontent.com/s/asbb5ofggd7zxig/Banner_smartphone_2.png?dl=0',
+      },
+    ];
+export const SlidesTab = [
+      {
+        image:
+          'https://dl.dropboxusercontent.com/s/kuxcs934gk4aur2/Banner_tablet_1.png?dl=0',
+      },
+      {
+        image:
+          'https://dl.dropboxusercontent.com/s/x5n9v1xgqf4h98z/Banner_tablet_2.png?dl=0',
+      },
+    ];
+export const SlidesDesktop = [
+      {
+        image:
+          'https://dl.dropboxusercontent.com/s/w9eupyevsq43ocv/Banner_desktop_1.png?dl=0',
+      },
+      {
+        image:
+          'https://dl.dropboxusercontent.com/s/xqojzprggr1bpts/Banner_desktop_2.png?dl=0',
+      },
+    ];
