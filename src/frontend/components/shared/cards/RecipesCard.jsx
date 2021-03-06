c7080102dd7b15214a40d9ba5d79068ef8f4a0a0
Nahuel <37487823+NahuE3@users.noreply.github.com>
2021-03-06 01:38:25 -0300

    Arreglos

--- src/frontend/components/shared/cards/RecipesCard.jsx
+++ src/frontend/components/shared/cards/RecipesCard.jsx
@@ -101,9 +101,13 @@ const RecipesCard = ({ data, openRecipe, addFavorite, favorite }) => {
 const StyledCard = styled.div`
   width: 100%;
   min-width: 140px;
-  height: 400px;
+  height: 300px;
   border-radius: var(--normal-radius);
   box-shadow: var(--card-shadow);
+
+  ${media.tab} {
+    height: 400px;
+  }
 `;
 const StyledImgSection = styled(Link)`
   display: flex;
