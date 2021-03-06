ea978e4e05efa920146b692ca566132ebd6a9829
Antonio <aguzmanv09@gmail.com>
2021-03-05 18:00:02 -0600

    Cambiando ruta del build

--- esbuild.js
+++ esbuild.js
@@ -5,14 +5,14 @@ esbuild.build({
   bundle: true,
   platform: 'node',
   target: ['node14.15'],
-  outfile: 'dist/index.js',
+  outfile: 'public/index.js',
   entryPoints: ['src/server/index.jsx'],
 }).catch((e) => console.error(e.message));
 
 esbuild.build({
   minify: true,
   bundle: true,
-  outfile: 'dist/public/app.js',
+  outfile: 'public/public/app.js',
   entryPoints: ['src/frontend/index.jsx'],
   define: {
     'process.env.NODE_ENV': '"production"',
